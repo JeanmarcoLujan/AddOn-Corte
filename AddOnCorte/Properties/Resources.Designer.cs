a66985f12cf1a653adb4466052e87795658af200
Jeanmarco lujan ventura <mark.15@hotmail.es>
2024-03-03 21:53:11 -0500

    v1.0.1.5 - Compilar e instalar version

--- AddOnCorte/Properties/Resources.Designer.cs
+++ AddOnCorte/Properties/Resources.Designer.cs
@@ -88,7 +88,7 @@ namespace AddOnCorte.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.0.1.3.
+        ///   Looks up a localized string similar to 1.0.1.5.
         /// </summary>
         internal static string VersionAddon {
             get {
